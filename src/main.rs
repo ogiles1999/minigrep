c1c8f03407b70be3cf9317ee4a46dbb30828c00e
Oliver Giles <ogiles@email.com>
2022-09-28 16:31:28 +0100

    fixed messed up closing bracket in main

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
-    };
+    });
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {e}");
